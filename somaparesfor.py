fa546c9b67700a9c635f7ebc44f2deb669aa32f5
zFreshy <91271353+zFreshy@users.noreply.github.com>
2024-10-25 14:27:56 -0300

    Add files via upload

--- somaparesfor.py
+++ somaparesfor.py
@@ -1,14 +1,12 @@
-# Solicitar os limites ao usuário
+#Matheus Vinícius
 limite_inferior = int(input("Digite o limite maior: "))
 limite_superior = int(input("Digite o limite menor: "))
 
-# Garantir que o limite inferior é maior que o superior
 while limite_inferior <= limite_superior:
     print("O primeiro número deve ser maior que o segundo. Tente novamente.")
     limite_inferior = int(input("Digite o limite maior: "))
     limite_superior = int(input("Digite o limite menor: "))
 
-# Calcular a soma dos números pares
 soma = 0
 for i in range(limite_superior, limite_inferior + 1):
     if i % 2 == 0:
--- tabuadafor.py
+++ tabuadafor.py
@@ -1,3 +1,4 @@
+#Matheus Vinícius
 numero = int(input("Digite um número para ver a tabuada: "))
 for i in range(1, 11):
     print(f"{numero} x {i} = {numero * i}")
