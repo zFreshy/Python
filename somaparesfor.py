56529d54a09f3fbc3de9ed75341a85f98b9a08ff
zFreshy <91271353+zFreshy@users.noreply.github.com>
2024-10-25 14:32:35 -0300

    Add files via upload

--- somaparesfor.py
+++ somaparesfor.py
@@ -1,15 +1,13 @@
 #Matheus Vinícius
-limite_inferior = int(input("Digite o limite maior: "))
-limite_superior = int(input("Digite o limite menor: "))
+limite1 = int(input("Digite o primeiro número: "))
+limite2 = int(input("Digite o segundo número: "))
 
-while limite_inferior <= limite_superior:
-    print("O primeiro número deve ser maior que o segundo. Tente novamente.")
-    limite_inferior = int(input("Digite o limite maior: "))
-    limite_superior = int(input("Digite o limite menor: "))
+limiteInferior = min(limite1, limite2)
+limiteSuperior = max(limite1, limite2)
 
 soma = 0
-for i in range(limite_superior, limite_inferior + 1):
+for i in range(limiteInferior, limiteSuperior + 1):
     if i % 2 == 0:
         soma += i
 
-print(f"A soma dos números pares entre {limite_superior} e {limite_inferior} é: {soma}")
+print(f"A soma dos números pares entre {limiteInferior} e {limiteSuperior} é: {soma}")
